0d1c6d988577f9577c982993f4786b5162c7b3e4
ssekimoto <ssekimoto@vmware.com>
2019-04-12 23:03:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clarity-light-theme-v012-4uadmq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/clarity-light-theme-v012-4uadmq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { clipboardModule } from 'angular-clipboard';
+
+
+@Component({
+  selector: 'my-app',
+  styleUrls: ['app.component.css'],
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+
+}
